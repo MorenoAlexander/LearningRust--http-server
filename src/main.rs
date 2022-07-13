c49bc760c13dc61bab92f9b7eabeacfa18a88de6
morenoalexander <am@alexandermoreno.com>
2022-07-12 19:43:39 -0500

    added Method enum, request struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 
 
 fn main() {
+
     let server = Server::new(String::from("127.0.0.1:8080"));
     server.run();
 }
@@ -21,3 +22,28 @@ impl Server {
         println!("Listening on {}", self.addr);
     }
 }
+
+enum Method {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH 
+}
+
+struct Request {
+    path: String,
+    method: Option<String>,
+    query_string: Method,
+
+}
+
+/*
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+ */
