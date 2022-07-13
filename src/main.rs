62bde25540c60035154eb4889764bd7bf50f13bd
morenoalexander <am@alexandermoreno.com>
2022-07-12 20:11:15 -0500

    separated files into modules

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,54 @@
-
+use http::{method::*, request::*};
+use server::Server;
 
 fn main() {
-
     let server = Server::new(String::from("127.0.0.1:8080"));
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
+mod server {
 
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr,
+    pub struct Server {
+        addr: String,
+    }
+
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Self { addr }
         }
 
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.addr);
+        pub fn run(self) {
+            println!("Listening on {}", self.addr);
+        }
     }
 }
 
-enum Method {
-    GET,
-    POST,
-    PUT,
-    DELETE,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH 
-}
+mod http {
 
-struct Request {
-    path: String,
-    method: Option<String>,
-    query_string: Method,
+    pub mod method {
 
+        pub enum Method {
+            GET,
+            POST,
+            PUT,
+            DELETE,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
+        }
+    }
+
+    pub mod request {
+        use super::method::Method;
+
+        pub struct Request {
+            path: String,
+            method: Option<String>,
+            query_string: Method,
+        }
+    }
 }
 
 /*
