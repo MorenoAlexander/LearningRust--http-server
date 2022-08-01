38aaa1dd6e05e63ed487e4c94d4af815b49c6986
MorenoAlexander <am@alexandermoreno.com>
2022-08-01 01:20:55 -0500

    added FromStr impl to Method enum

--- src/http/method.rs
+++ src/http/method.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 
 pub enum Method {
     GET,
@@ -10,3 +11,18 @@ pub enum Method {
     TRACE,
     PATCH,
 }
+
+impl FromStr for Method {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+        GET => Ok(Self::GET),
+        DELETE => Ok(Self::DELETE),
+        POST => Ok(Self::POST),
+        PUT => Ok(Self::PUT)),
+        HEAD => Ok(Self::HEAD),
+        CONNECT => OK(Self::CONNECT),
+        _ => Err((String::from("error),
+    {
+}
+        
