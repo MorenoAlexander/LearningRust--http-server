e3f1a41ed95e40216c25dedf8036060b3a7e0280
MorenoAlexander <am@alexandermoreno.com>
2022-08-01 01:20:18 -0500

    removed unused imports for main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
-use http::Method;
-use http::Request;
 use server::Server;
 
 mod http;
+
 mod server;
 
 fn main() {
     let server = Server::new(String::from("127.0.0.1:8080"));
     server.run();
 }
-
-/*
-GET /user?id=10 HTTP/1.1\r\n
-HEADERS \r\n
-BODY
- */
