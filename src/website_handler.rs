e84967b39689138db6edda31a094b720ec305618
morenoalexander <am@alexandermoreno.com>
2022-08-24 11:05:49 -0500

    added launch settings and website_handler match

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -5,7 +5,8 @@ pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, req: &Request) -> Response {
-        dbg!(req);
-        Response::new(StatusCode::Ok, Some(String::from("<h1>Hello World</h1>")))
+        match req.method() {
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
