7e72024d2ebf4343f4d54ed0268ef753780230bf
morenoalexander <am@alexandermoreno.com>
2022-08-25 00:00:21 -0500

    updated website_handler

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,11 +1,33 @@
-use super::http::{Request, Response, StatusCode};
+use super::http::{Method, Request, Response, StatusCode};
 use super::server::Handler;
+use std::fs;
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+    public_path: String,
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, req: &Request) -> Response {
         match req.method() {
+            &Method::GET => match req.path() {
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents.to_string())),
+                    None => Response::new(StatusCode::NotFound, None),
+                },
+            },
             _ => Response::new(StatusCode::NotFound, None),
         }
     }
